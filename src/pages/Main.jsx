a01115faa18f88691d61d0834e3fa9b19d6578b2
jonghoon1978 <86106738+jonghoon1978@users.noreply.github.com>
2021-12-13 15:39:39 +0900

    이미지버튼

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,22 +1,27 @@
-import { Button, Grid, Input, Text } from '../elements'
+import { Button, Grid, Input, Text } from "../elements";
 
 const Main = (props) => {
   return (
     <>
       <Grid bg="#fff">
         <Grid margin="10px" bg="#fff">
-          <Button>jiSSS</Button>
+          <Button
+             >
+            jiSSS
+          </Button>
         </Grid>
 
         <Grid margin="10px" bg="#fff">
           <Text>Text!</Text>
         </Grid>
         <Grid margin="10px" bg={({ theme }) => theme.color.background}>
-          <Input label="Input here!" multiLine placeholder="asd">Input!</Input>
+          <Input label="Input here!" multiLine placeholder="asd">
+            Input!
+          </Input>
         </Grid>
       </Grid>
     </>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
